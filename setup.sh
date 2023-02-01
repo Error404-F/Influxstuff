e8b87f229371bb83911b08800393de90e609a63b
Karol Krizka <kkrizka@gmail.com>
2023-02-01 09:38:15 +0000

    Add optional setup.sh argument to select configuration.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+SETUP=main
+if [ ${#} == 1 ]; then
+    SETUP=${1}
+fi
+
 export WORKDIR=$(realpath $(dirname ${BASH_SOURCE[0]}))
 
 # Powertool utilities
@@ -6,7 +11,7 @@ export LD_LIBRARY_PATH=${WORKDIR}/powertools/build/lib:${LD_LIBRARY_PATH}
 
 # ITSDAQ configuration files
 export SCTDAQ_ROOT=${WORKDIR}/itsdaq-sw
-export SCTDAQ_VAR=${WORKDIR}/itsdaq-cfg/main
+export SCTDAQ_VAR=${WORKDIR}/itsdaq-cfg/${SETUP}
 export SCTDB_USER=FU
 
 # Database
@@ -14,6 +19,4 @@ if [ -e ${WORKDIR}/setup_db.sh ]; then
     source ${WORKDIR}/setup_db.sh
 fi
 
-name=$(basename ${SCTDAQ_VAR})
-
-export PS1="(${name}) ${PS1}"
+export PS1="(${SETUP}) ${PS1}"
